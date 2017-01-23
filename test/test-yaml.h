1d2aada6d2c665519768b6edb3f48317c224b439
Trung Pham <phambaotrung@gmail.com>
2017-01-22 22:59:09 -0800

    Test: add include to remove warning

--- test/test-yaml.h
+++ test/test-yaml.h
@@ -1,37 +1,38 @@
 /***************************************************************************
- * Copyright (C) 2017 Trung Pham <me@trungbpham.com>                       *           
- * All rights reserved.                                                    *           
- *                                                                         *           
- * Redistribution and use in source and binary forms, with or without      *           
+ * Copyright (C) 2017 Trung Pham <me@trungbpham.com>                       *
+ * All rights reserved.                                                    *
+ *                                                                         *
+ * Redistribution and use in source and binary forms, with or without      *
  * modification, are permitted provided that the following conditions      *
  * are met:                                                                *
- *                                                                         *           
+ *                                                                         *
  *    * Redistributions of source code must retain the above copyright     *
- *    notice, this list of conditions and the following disclaimer.        *                
- *                                                                         *           
+ *    notice, this list of conditions and the following disclaimer.        *
+ *                                                                         *
  *    * Redistributions in binary form must reproduce the above copyright  *
  *    notice, this list of conditions and the following disclaimer in the  *
- *    documentation and/or other materials provided with the distribution. *                      
- *                                                                         *           
+ *    documentation and/or other materials provided with the distribution. *
+ *                                                                         *
  *    * Neither the name of cfeap nor the names of its contributors may    *
  *    be used to endorse or promote products derived from this software    *
- *    without specific prior written permission.                           *                         
- *                                                                         *           
+ *    without specific prior written permission.                           *
+ *                                                                         *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS     *
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT       *   
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT       *
  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR   *
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT    * 
- * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,  *  
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT    *
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,  *
  * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT        *
  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,   *
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY   * 
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT     * 
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE   *   
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.    *           
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY   *
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT     *
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE   *
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.    *
  ***************************************************************************/
 
 #include <stdio.h>
 
+#include "sput.h"
 #include "yaml/yaml.h"
 
 static void test_yaml();
@@ -63,20 +64,20 @@ static void test_yaml_case_a()
 
     /* Set input file */
     yaml_parser_set_input_file(&parser, fp);
-    
+
     int state = 0;
     int count_id = 0;
     int expected_num_id = 3;
     char *tk;
-    
+
     do {
         yaml_parser_scan(&parser, &token);
         switch(token.type) {
-            case YAML_KEY_TOKEN:     
+            case YAML_KEY_TOKEN:
                 state = 0;
                 break;
-            case YAML_VALUE_TOKEN:   
-                state = 1; 
+            case YAML_VALUE_TOKEN:
+                state = 1;
                 break;
             case YAML_SCALAR_TOKEN:
                 tk = (char *)token.data.scalar.value;
@@ -84,18 +85,18 @@ static void test_yaml_case_a()
                     if(!strcmp(tk, "id")) {
                         count_id += 1;
                     }
-                } 
+                }
                 break;
-           default: 
+           default:
                break;
         }
         if(token.type != YAML_STREAM_END_TOKEN) {
             yaml_token_delete(&token);
         }
     } while(token.type != YAML_STREAM_END_TOKEN);
-        
+
     sput_fail_if(count_id != expected_num_id, "Case a: Numbers of id");
-    
+
     yaml_token_delete(&token);
     yaml_parser_delete(&parser);
     fclose(fp);
@@ -117,7 +118,7 @@ static void test_yaml_case_b()
 
     /* Set input file */
     yaml_parser_set_input_file(&parser, fp);
-    
+
     int done = 0;
     while (!done)
     {
@@ -132,7 +133,7 @@ static void test_yaml_case_b()
 
         yaml_document_delete(&document);
     }
-    
+
     yaml_parser_delete(&parser);
     fclose(fp);
 }
@@ -151,7 +152,7 @@ void print_yaml_node(yaml_document_t *document, yaml_node_t *node)
     char *node_value;
     yaml_node_t *material;
     yaml_node_t *next_node_p;
-    
+
     switch (node->type) {
         case YAML_NO_NODE:
             break;
